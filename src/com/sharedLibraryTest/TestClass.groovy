9fd7985eb0ffa4fdfdff7ad0c62a7866decd113d
Kashika <40196858+kashikakhatri08@users.noreply.github.com>
2021-06-25 18:02:06 +0530

    Update TestClass.groovy

--- src/com/sharedLibraryTest/TestClass.groovy
+++ src/com/sharedLibraryTest/TestClass.groovy
@@ -2,10 +2,10 @@ package com.sharedLibraryTest
 
 class TestClass {
     def sayHi() {
-        echo "Hi from ClassTest!"
+        println "Hi from ClassTest!"
     }
 
     def sayHello(name) {
-        echo "Hello ${name}"
+        println "Hello ${name}"
     }
 }
