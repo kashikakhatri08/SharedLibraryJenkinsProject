d90a984b43a0b83ac5de48849e4fe199809d17d7
Kashika <40196858+kashikakhatri08@users.noreply.github.com>
2021-06-26 16:21:29 +0530

    Update BuildClass.groovy

--- src/com/sharedLibraryTest/BuildClass.groovy
+++ src/com/sharedLibraryTest/BuildClass.groovy
@@ -3,10 +3,10 @@ package com.sharedLibraryTest
 class BuildClass {
     Script script;
     def EvenBuild(JobName,BuildNumber) {
-        script.echo("${JobName} Current Build is Even and the build Number is ${BuildNumber}")      
+        script.echo("${JobName} Job's Current Build is Even and the build Number is ${BuildNumber}")      
     }
 
     def OddBuild(JobName,BuildNumber) {
-        script.echo("${JobName} Current Build is Odd and the build Number is ${BuildNumber}")
+        script.echo("${JobName} Job's Current Build is Odd and the build Number is ${BuildNumber}")
     }
 }
